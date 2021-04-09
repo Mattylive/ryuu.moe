ef6d3c3194e767403bf601a5ac59b7e8aea570d4
Matty <mc360masteryt@gmail.com>
2021-04-09 19:49:53 +0100

    added pronouns and changed link colours

---
+++ assets/js/age.js
@@ -0,0 +1,26 @@
+var DOB = "December 15, 2004";
+var millisecondsBetweenDOBAnd1970 = Date.parse(DOB);
+var millisecondsBetweenNowAnd1970 = Date.now();
+var ageInMilliseconds = millisecondsBetweenNowAnd1970-millisecondsBetweenDOBAnd1970;
+
+  var milliseconds = ageInMilliseconds;
+  var second = 1000;
+  var minute = second*60;
+  var hour = minute*60;
+  var day = hour*24;
+  var month = day*30;
+  var year = day*365;
+
+var years = Math.round(milliseconds/year);
+var months = years*12;
+var days = years*365;
+var hours = Math.round(milliseconds/hour);
+var seconds = Math.round(milliseconds/second);
+
+
+function printResults(){
+  var message = ""+years+"."+milliseconds+""
+  document.getElementById('age').innerHTML = message;
+}
+
+window.onload = printResults;
